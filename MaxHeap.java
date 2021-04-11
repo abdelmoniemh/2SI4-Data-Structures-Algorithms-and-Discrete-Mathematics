222b1f454afff5693c082df17a2e37a0ddb354fd
tekkersss1 <abdelmoniemmmh@gmail.com>
2021-04-11 16:38:10 -0400

    Passes all test cases

--- MaxHeap.java
+++ MaxHeap.java
@@ -1,17 +1,15 @@
-import org.graalvm.compiler.nodes.extended.NullCheckNode;
-
 public class MaxHeap {
     private Integer[] Heap;
     private int heapSize;
     private int arraySize;
 
-    public MaxHeap(int n){
+    public MaxHeap(int n){ // Time : O(1) // Space : S(n)
         Heap = new Integer[2*n];
         arraySize = n;
         heapSize = 0;
     }
 
-    public MaxHeap(Integer[] array){
+    public MaxHeap(Integer[] array){ // Time : O(nlogn) // Space : S(n)
         arraySize = 2*array.length;
         Heap = new Integer[arraySize];
         for (Integer i:array){
@@ -20,21 +18,33 @@ public class MaxHeap {
 
     }
 
-    public void insert(int n){
+    public void insert(int n){ // Average Time : Log(n) Worst Case Time : O(n) // Average Space : 1 Worst Case Space : S(n)
         if ((heapSize+1)<arraySize){
             Heap[heapSize] = (Integer)n;
             heapSize++;
-            heapifyUp();
+
+            int index = heapSize-1;
+            while(hasParent(index) && Heap[index]>getParent(index)){
+                swap(index, getParentIndex(index));
+                index = getParentIndex(index);
+            }
+
         } else {
             resize();
             Heap[heapSize] = (Integer)n;
             heapSize++;
-            heapifyUp();
+
+            int index = heapSize-1;
+            while(hasParent(index) && Heap[index]>getParent(index)){
+                swap(index, getParentIndex(index));
+                index = getParentIndex(index);
+            }
+
         }
 
     }
     
-    private void resize(){
+    private void resize(){ 
         Integer[] temp = new Integer[2*arraySize];
         for (int i =0;i<heapSize;i++){
             temp[i] = Heap[i];
@@ -43,11 +53,26 @@ public class MaxHeap {
         arraySize = temp.length;
     }
 
-    private int deleteMax(){
+    private int deleteMax(){ // Time: Log(n) // Space: 1 
         int maxValue = Heap[0].intValue();
         swap(0, heapSize-1);
         Heap[heapSize-1] = null;
-        heapifyDown();
+
+        int index = 0;
+        while (hasLeftChild(index)){
+            int biggerChildIndex = getLeftChildIndex(index);
+            if (hasRightChild(index) && (getRightChild(index) > Heap[biggerChildIndex]))
+                biggerChildIndex = getRightChildIndex(index);
+            
+            if (Heap[index] > Heap[biggerChildIndex]){
+                break;
+            } else {
+                if (Heap[biggerChildIndex] != null)
+                    swap(index, biggerChildIndex);
+            }
+            index = biggerChildIndex;    
+        }
+
         heapSize--;
         return maxValue;
 
@@ -61,16 +86,11 @@ public class MaxHeap {
 
     }
 
-    public static void heapsort(Integer[] arrayToSort){
+    public static void heapsort(Integer[] arrayToSort){ // Time: O(nlogn)
         MaxHeap newHeap = new MaxHeap(arrayToSort);
         int newHeapSize = newHeap.getSizeHeap();
-        Integer[] temp = new Integer[newHeapSize];
         for (int i = 0;i<newHeapSize;i++){
-            //System.out.println(i);
-            temp[i] = Integer.valueOf(newHeap.deleteMax());
-        }
-        for (int i = 0;i<newHeapSize;i++){
-            arrayToSort[i] = temp[i];
+            arrayToSort[i] = Integer.valueOf(newHeap.deleteMax());
         }
     }
 
@@ -86,31 +106,6 @@ public class MaxHeap {
         return Heap;
     }
 
-    public void heapifyUp(){
-        int index = heapSize-1;
-        while(hasParent(index) && Heap[index]>getParent(index)){
-            swap(index, getParentIndex(index));
-            index = getParentIndex(index);
-        }
-    }
-
-    public void heapifyDown(){
-        int index = 0;
-        while (hasLeftChild(index)){
-            int biggerChildIndex = getLeftChildIndex(index);
-            if (hasRightChild(index) && (getRightChild(index) > Heap[biggerChildIndex]))
-                biggerChildIndex = getRightChildIndex(index);
-            
-            if (Heap[index] > Heap[biggerChildIndex]){
-                break;
-            } else {
-                if (Heap[biggerChildIndex] != null)
-                    swap(index, biggerChildIndex);
-            }
-            index = biggerChildIndex;    
-        }
-        
-    }
 
     public void swap(int indexX, int indexY){
         Integer temp = Heap[indexX];
